b6c74580a36de1cfd5a115d953146fecc451667f
JinPingOu <gina910805@gmail.com>
2023-06-14 04:48:18 +0800

    2023/06/14 - JinPing - test

--- controllers/visitor.js
+++ controllers/visitor.js
@@ -4,33 +4,34 @@ const public = {
     postVisitApply: async (req, res) => {
         visitor.insertVisitor(req.body.guest_ID, req.body.dorm_name, 
             req.body.visit_date, req.body.visit_approve);
-        res.redirect('visitor/Info');
+        res.redirect('/visitor/Info');
     },
 
     getVisitList: async (req, res) => {
         const visitorInfo = await visitor.selectAllVisitor();
+        console.log(visitorInfo);
         res.render('visitorInfo', {visitorInfo});
     },
 
     getVisitInfo: async (req, res) => {
-        const visitorInfo = await visitor.selectVisitor(req.body.guest_ID);
-        res.render('visitorInfo', {visitorInfo});
+        const visitorInfos = await visitor.selectVisitor(req.body.guest_ID);
+        res.render('visitorInfos', visitorInfos);
     },
 
     postVisitApprove: async (req, res) => {
         visitor.approveVisit(req.body.guest_ID);
-        res.redirect('visitor/list');
+        res.redirect('/visitor/list');
     },
 
     postVisitModify: async (req, res) => {
         visitor.modifyVisitor(req.body.guest_ID, req.body.dorm_name, 
             req.body.visit_date, req.body.visit_approve);
-        res.redirect('visitor/Info');
+        res.redirect('/visitor/Info');
     },
 
     postVisitDelete: async (req, res) => {
         visitor.deleteVisitor(req.body.guest_ID);
-        res.redirect('visitor/');
+        res.redirect('/visitor/list');
     }
 
 }

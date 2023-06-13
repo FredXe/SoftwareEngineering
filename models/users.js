082d11555114fe4a4732fbdab4770afc08665162
Chenghuexin <cindy2090.cheng@gmail.com>
2023-06-13 21:43:56 +0800

    add new model users and modify userInfo.js

---
+++ models/users.js
@@ -0,0 +1,107 @@
+const db = require('./db');
+const fs = require('fs/promises')
+const utils = require('./utils');
+
+const public = {
+
+    insertAdmin: async function(user_ID , user_name , sex , password , email , eroll_year , phnumber) {
+        const insertUsers = `insert users ('${user_ID}' , '${user_name}' , ${sex} , '${password}' , '${email}' , '${eroll_year}' , '${phnumber}');`
+        const insertAdmin = `insert admin ('${user_ID}')`;
+
+        try {
+			await db.query(insertUsers);
+            await db.query(insertAdmin);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+
+    insertHousemaster: async function(user_ID , user_name , sex , password , email , eroll_year , phnumber) {
+        const insertUsers = `insert users ('${user_ID}' , '${user_name}' , ${sex} , '${password}' , '${email}' , '${eroll_year}' , '${phnumber}');`
+        const insertHousemaster = `insert housemaster ('${user_ID}')`;
+
+        try {
+			await db.query(insertUsers);
+            await db.query(insertHousemaster);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+
+    insertMaintainer: async function(user_ID , user_name , sex , password , email , eroll_year , phnumber) {
+        const insertUsers = `insert users ('${user_ID}' , '${user_name}' , ${sex} , '${password}' , '${email}' , '${eroll_year}' , '${phnumber}');`
+        const insertMaintainer = `insert maintainer ('${user_ID}')`;
+
+        try {
+			await db.query(insertUsers);
+            await db.query(insertMaintainer);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+
+    insertnonResidentStudent: async function(user_ID , user_name , sex , password , email , eroll_year , phnumber) {
+        const insertUsers = `insert users ('${user_ID}' , '${user_name}' , ${sex} , '${password}' , '${email}' , '${eroll_year}' , '${phnumber}');`
+        const insertStudent = `insert maintainer ('${user_ID}')`;
+        const insertnonResidentStudent = `insert non_resident_student ('${user_ID}')`;
+        try {
+			await db.query(insertUsers);
+            await db.query(insertStudent);
+            await db.query(insertnonResidentStudent);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+
+    delAdmin: async function(user_ID) {
+        const delAdmin = `delete from admin where user_ID = '${user_ID}';`;
+        const deltUsers = `delete from users where user_ID = '${user_ID}';`;
+        
+
+        try {
+			await db.query(delAdmin);
+            await db.query(deltUsers);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+
+    delHousemaster: async function(user_ID) {
+        const delHousemaster = `delete from housemaster where user_ID = '${user_ID}';`;
+        const deltUsers = `delete from users where user_ID = '${user_ID}';`;
+        
+
+        try {
+			await db.query(delAdmin);
+            await db.query(deltUsers);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+
+    delMaintainer: async function(user_ID) {
+        const delMaintainer = `delete from maintainer where user_ID = '${user_ID}';`;
+        const deltUsers = `delete from users where user_ID = '${user_ID}';`;
+        
+
+        try {
+			await db.query(delMaintainer);
+            await db.query(deltUsers);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+
+    delnonResidentStudent: async function(user_ID) {
+        const delnonResidentStudent = `delete from non_resident_student where user_ID = '${user_ID}';`;
+        const deltUsers = `delete from users where user_ID = '${user_ID}';`;
+        
+
+        try {
+			await db.query(delnonResidentStudent);
+            await db.query(deltUsers);
+		} catch (err) {
+			console.error(err);
+		}
+    },
+}
